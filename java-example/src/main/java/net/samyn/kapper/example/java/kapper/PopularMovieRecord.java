cfdf6ef56a071657ffe23597f99d1576080a594c
Dries Samyn <5557551+driessamyn@users.noreply.github.com>
2026-01-01 17:14:44 +0100

    migrate examples to main repo (#118)

--- java-example/src/main/java/net/samyn/kapper/example/java/kapper/PopularMovieRecord.java
+++
@@ -1,9 +0,0 @@
-package net.samyn.kapper.example.java.kapper;
-
-public record PopularMovieRecord(
-        String title,
-        long grossed,
-        double comparedToAnnualAverage,
-        int allTimeRanking
-) {}
-
